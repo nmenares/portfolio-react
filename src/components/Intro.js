425f428c788442c5c44076c97a59d8900566bcc7
Nataly Menares <natalymenares@Natalys-MacBook-Pro.local>
2022-12-05 15:08:19 -0800

    fix intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,12 +1,12 @@
 import React from "react";
-import "../styles/navbar.css";
+import "../styles/intro.css";
 import { texts } from "../utils/Texts.js";
 
 const Intro = () => {
   return (
-    <div className="ui grid">
+    <div className="ui grid intro-wrapper">
       <div className="eight wide column"></div>
-      <div className="eight wide column intro-text">{texts.intro}</div>
+      <div className="seven wide column intro-text">{texts.intro}</div>
     </div>
   );
 };
